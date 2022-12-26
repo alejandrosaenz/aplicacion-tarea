f74597483008a872cf0dbe9d7fb8df10d569923a
alejandrosaenz <49048411+alejandrosaenz@users.noreply.github.com>
2022-12-26 15:23:06 -0500

    Maquetación para la tabla tareas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Tarea from './componentes/Tarea.js';
 
 function App() {
   return (
-    <div className='Aplicacion-tareas'>
-      React
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo' 
+        />     
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <Tarea texto='Aprender React'/>
+      </div>  
     </div>
   );
 }
